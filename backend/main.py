430f18b45d4ca1290b7cb8239357d3617c2e7dc9
CelioSlomp <slompcelio132@gmail.com>
2021-11-11 21:47:53 -0300

    Modificado o Sobrenos

--- backend/main.py
+++ backend/main.py
@@ -128,27 +128,6 @@ if __name__ == '__main__':
         # adicionar cabeçalho de liberação de origem
         resposta.headers.add("Access-Control-Allow-Origin", "")
         return resposta
-
-    @app.route("/incluir_empresa", methods=['post'])
-    def incluir_empresa():
-        # preparar uma resposta otimista
-        resposta = jsonify({"resultado": "ok", "detalhes": "ok"})
-        # receber as informações da nova empresa
-        dados = request.get_json()  # (force=True) dispensa Content-Type na requisição
-        try:  # tentar executar a operação
-            nova = Empresa(**dados)  # criar a nova pessoa
-            db.session.add(nova)  # adicionar no BD
-            db.session.commit()  # efetivar a operação de gravação
-        except Exception as e:  # em caso de erro...
-            # informar mensagem de erro
-            resposta = jsonify({"resultado": "erro", "detalhes": str(e)})
-        # adicionar cabeçalho de liberação de origem
-        resposta.headers.add("Access-Control-Allow-Origin", "")
-        return resposta
-    '''
-    Métodos da AV3
-    
-    
     
     # Lista todas as empresas cadastradas
     @app.route("/listar_empresas")
@@ -172,7 +151,6 @@ if __name__ == '__main__':
             retorno.append(i.json())
 
         return jsonify(retorno)
-    '''
 
     # Inicia o servidor
     app.run(debug=True)
